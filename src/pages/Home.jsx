b9d26593fa7b8e73a9e7f4ad9857ded5d5f18b01
Franco Agostinelli <francoagostinelli2000@gmail.com>
2025-05-15 18:04:04 -0300

    mostrar cartas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,42 @@
-// pages/Home.jsx
-import React from 'react';
-export default function Home() {
-return <h1>Bienvenido a la tienda Pokémon</h1>;
+import { useEffect, useState } from 'react';
+import '../styles/Home.css';
+function Home() {
+  const [cards, setCards] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`https://api.pokemontcg.io/v2/cards?q=set.name:"Prismatic Evolutions"`)
+      .then(res => res.json())
+      .then(data => {
+        setCards(data.data); // la propiedad es "data"
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error('Error fetching cards:', err);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Cargando cartas...</p>;
+
+  return (
+    <div className="card-grid">
+  {cards
+  .filter(card => card.tcgplayer?.prices?.holofoil?.market)
+  .sort((a, b) =>
+  b.tcgplayer.prices.holofoil.market - a.tcgplayer.prices.holofoil.market)
+  .map(card => (
+    <div key={card.id} className="card">
+      <img src={card.images.small} alt={card.name} />
+      <h2>{card.name}</h2>
+      <p>
+        Precio: ${card.tcgplayer.prices.holofoil.market.toFixed(2)}
+      </p>
+    </div>
+))}
+</div>
+
+  );
 }
+
+export default Home;
