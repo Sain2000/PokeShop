bdf2cab908c55473b3b6ab01a365330e63460f4a
Franco Agostinelli <francoagostinelli2000@gmail.com>
2025-05-16 16:47:16 -0300

    nuevo icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
 import { Link } from 'react-router-dom';
+import masterballIcon from '../assets/masterball.png'; // tu icono
 
 function Navbar() {
   const { totalItems, totalPrice } = useContext(CartContext);
@@ -8,7 +9,17 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">PokéStore</Link>
+        <Link className="navbar-brand d-flex align-items-center" to="/">
+          <img
+            src={masterballIcon}
+            alt="Master Ball"
+            width="30"
+            height="30"
+            className="me-2"
+            style={{ objectFit: 'contain' }}
+          />
+          PokéStore
+        </Link>
 
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Optional: for navbar togg
 import { BrowserRouter as Router } from 'react-router-dom';
 import { CartProvider } from './context/CartContext'; // Adjust path if needed
 
+import './index.css';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
